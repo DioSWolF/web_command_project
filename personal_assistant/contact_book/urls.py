2affd80815c6bfd7a9e1cae9fac3bf20d9c0651a
IvanShkvyr <Gis2011I@gmail.com>
2023-02-04 22:12:01 +0200

    Add views and html files

--- personal_assistant/contact_book/urls.py
+++ personal_assistant/contact_book/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = "contact_book"
 
 urlpatterns = [
-    path('checker/', views.checker, name="checker"),
+    path("checker/", views.checker, name="checker"),
+    path("", views.get_contact_book, name="contacts_book"),
+    path("create/", views.create_contact, name="create_contact"),
+    path("change/", views.change_contact, name="change_contact"),
+    path("find/", views.find_by, name="find_contact"),
+    path("delete/", views.delete_contact, name="delete_contact"),
+    path("birthday/", views.get_birthday_contacts, name="get_birthday_contacts"),
 ]
--- personal_assistant/contact_book/views.py
+++ personal_assistant/contact_book/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render
 def checker(request):
     return render(request, 'contact_book/checker.html', context={"msg": "Good news!!! It is working)"})
 
+
+def get_contact_book(request):
+    return render(request, 'contact_book/main_contacts_book.html', context={"msg": "MAIN"})
+
+
+def create_contact(request):
+    return render(request, 'contact_book/create_contact.html', context={"msg": "CREATE"})
+
+
+def change_contact(request):
+    return render(request, 'contact_book/change_contact.html', context={"msg": "CHANGE"})
+
+
+def find_by(request):
+    return render(request, 'contact_book/find_by.html', context={"msg": "FIND BY"})
+
+
+def delete_contact(request):
+    return render(request, 'contact_book/delete_contact.html', context={"msg": "DELETE"})
+
+
+def get_birthday_contacts(request):
+    return render(request, 'contact_book/birthday_contacts.html', context={"msg": "GET BIRTHDAY CONTACTS"})
