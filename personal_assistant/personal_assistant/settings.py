46b738abfa58f3d0d577bb4ef375b3b41851588e
DioSWolF <106728732+DioSWolF@users.noreply.github.com>
2023-02-04 21:42:12 +0200

    Update settings.py

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -79,19 +79,14 @@ WSGI_APPLICATION = 'personal_assistant.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'command_project',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '1234321',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432'
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'command_project',
+         'USER': 'postgres',
+         'PASSWORD': '1234321',
+         'HOST': '127.0.0.1',
+         'PORT': '5432'
+     }
 }
 
 # Password validation
@@ -128,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
