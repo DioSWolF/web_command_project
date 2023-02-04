185370b6d0dae25eb7881fcd9394c426da4b13c4
Musfer <adzhymambetov@gmail.com>
2023-02-04 18:43:16 +0200

    Initail commit of file_storage.

--- personal_assistant/file_storage/models.py
+++ personal_assistant/file_storage/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# from PIL import Image
 
-# Create your models here.
+data_types = {("MP3", "OGG", "VORBIS", "FLAC", "WAV", "PCM", "AIFF", "AAC", "WMA", "FLAC", "ALAC"): "audio",
+              ("PNG", "JPG", "JPEG", "GIF", "SVG", "TIF", "TIFF", "EPS"): "image",
+              ("MP4", "MOV", "WMV", "AVI", "AVCHD", "FLV", "F4V", "SWF", "MKV", "WEBM", "HTML5", "MPEG-2",
+               "MPG", "MP2", "MPEG", "MPE", "MPV", "WMV", "AVCHD"): "video",
+              ("PDF", "DOC", "DOCX", "HTML", "HTM", "XLS", "XLSX", "TXT", "PPT", "PPTX", "ODP", "KEY"): "document"
+              }
+
+
+class File(models.Model):
+    data = models.FileField(upload_to='files/')
+    name = models.CharField(max_length=100, default="")
+
+    date_posted = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    datatype = models.CharField(max_length=10, default="other")
+
+    class Meta:
+        db_table = 'MediaStore'
+
+    def save(self, *args, **kwargs):
+        print(self.data)
+        extension = str(self.data).split(".")[-1]
+        for tpl, data_type in data_types.items():
+            if extension.upper() in tpl:
+                self.datatype = data_type
+                self.data.upload_to = f'{self.datatype}/'
+                break
+        # else:
+        #     self.datatype = "other"
+        print(f"filetype : {self.datatype}")
+        super().save(*args, **kwargs)
+        # if filetype == "image":
+        #     Image().save(*args, **kwargs)
+        # else:
+        #     super().save(*args, **kwargs)
+
+
+class Image(models.Model):
+    # data = models.FileField(upload_to='images/')
+    name = models.CharField(max_length=100, default='unknown image')
+    data = models.ImageField(upload_to='images/')
+    date_posted = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # owner
+
+    class Meta:
+        db_table = 'ImageStore'
+
+
+
+
+
--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,16 +79,20 @@ WSGI_APPLICATION = 'personal_assistant.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'command_project',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1234321',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432'
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'command_project',
-        'USER': 'postgres',
-        'PASSWORD': '1234321',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -124,7 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
