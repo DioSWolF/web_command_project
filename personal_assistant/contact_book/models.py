5b3ed8f6742f783724b3563ec97b121afb0b9b5c
IvanShkvyr <Gis2011I@gmail.com>
2023-02-08 21:20:43 +0200

    add all function to contact_book

--- personal_assistant/contact_book/models.py
+++ personal_assistant/contact_book/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class ContactBook(models.Model):
     first_name = models.CharField(max_length=25, null=False)
-    last_name = models.CharField(max_length=25)
-    phone = models.CharField(max_length=25)
-    email = models.CharField(max_length=25)
-    address = models.CharField(max_length=150)
-    description = models.CharField(max_length=150)
-    birthday = models.DateTimeField()
+    last_name = models.CharField(max_length=25, default=None)
+    phone = PhoneNumberField(blank=True)
+    email = models.EmailField(max_length=100, default=None)
+    address = models.CharField(max_length=150, default=None)
+    description = models.CharField(max_length=150, default=None)
+    birthday = models.DateField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
-    change_date = models.DateTimeField(default=timezone.now)
+    change_date = models.DateField(default=timezone.now)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
--- personal_assistant/contact_book/views.py
+++ personal_assistant/contact_book/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.db.models import Q
 
 from .forms import ContactForm
 from .models import ContactBook
@@ -14,7 +17,6 @@ def checker(request):
 
 
 def get_contact_book(request):
-
     return render(
         request,
         "contact_book/main_contacts_book.html",
@@ -27,7 +29,6 @@ def create_contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-
             return redirect(to="contact_book:contacts_book")
         else:
             return render(
@@ -40,7 +41,6 @@ def create_contact(request):
 
 def datail(request, contact_id):
     contact = get_object_or_404(ContactBook, pk=contact_id)
-
     return render(request, "contact_book/detail.html", context={"contact": contact})
 
 
@@ -60,36 +60,83 @@ def change_contact(request, contact_id):
                 birthday=request.POST["birthday"],
                 change_date=current_date,
             )
-
             return redirect(to="contact_book:contacts_book")
         else:
             return render(
                 request, "contact_book/change_contact.html", context={"form": form}
             )
-    birthday_str = contact.birthday.strftime("%Y-%m-%d")
-    print("---------------------------------------------")
-    print(birthday_str)
-    print("---------------------------------------------")
+    try:
+        birthday_str = contact.birthday.strftime("%Y-%m-%d")
+    except:
+        birthday_str = ""
+
     context = {"form": ContactForm(), "contact": contact, "birthday_str": birthday_str}
     return render(request, "contact_book/change_contact.html", context)
 
-    # contact = get_object_or_404(ContactBook, pk=contact_id)
-    # return render(request, 'contact_book/detail.html', context={'contact': contact})
-
 
 def find_by(request):
-    return render(request, "contact_book/find_by.html", context={"msg": "FIND BY"})
+    contacts = ContactBook.objects.all()
+    if request.method == "POST":
+
+        find_by_method = request.POST["find_by"]
+        search_value = (request.POST["search_value"])
+
+        if find_by_method == "first_name":
+            contacts = ContactBook.objects.filter(first_name__icontains=search_value)
+        elif find_by_method == "last_name":
+            contacts = ContactBook.objects.filter(last_name__icontains=search_value)
+        elif find_by_method == "phone":
+            contacts = ContactBook.objects.filter(phone__icontains=search_value)
+        elif find_by_method == "email":
+            contacts = ContactBook.objects.filter(email__icontains=search_value)
+        elif find_by_method == "address":
+            contacts = ContactBook.objects.filter(address__icontains=search_value)
+        elif find_by_method == "description":
+            contacts = ContactBook.objects.filter(description__icontains=search_value)
+        else:
+            contacts = ContactBook.objects.filter(Q(first_name__icontains=search_value) |\
+                                                  Q(last_name__icontains=search_value) |
+                                                  Q(phone__icontains=search_value) |\
+                                                  Q(email__icontains=search_value) |
+                                                  Q(address__icontains=search_value) |\
+                                                  Q(description__icontains=search_value))
+
+    return render(request, "contact_book/find_by.html", context={"contacts": contacts})
 
 
 def delete_contact(request, contact_id):
-    return render(
-        request, "contact_book/delete_contact.html", context={"msg": "DELETE"}
-    )
+    if request.POST:
+        ContactBook.objects.filter(pk=contact_id).delete()
+        return redirect(to="contact_book:contacts_book")
+    return render(request, "contact_book/delete_contact.html")
 
 
 def get_birthday_contacts(request):
-    return render(
-        request,
-        "contact_book/birthday_contacts.html",
-        context={"msg": "GET BIRTHDAY CONTACTS"},
-    )
+    current_date = datetime.now().date()
+    date_list = [current_date]
+    date_birthday_list = []
+
+    if request.method == "POST":
+        delta_date = int(request.POST["days_to"])
+
+        for i in range(delta_date + 1):
+            _date = current_date + timedelta(days=i)
+            date_list.append(_date)
+
+        for i in date_list:
+            context = ContactBook.objects.filter(birthday__month=i.month, birthday__day=i.day)
+            if context:
+                date_birthday_list.append(context)
+
+        return render(request, "contact_book/birthday_contacts.html", context={"contacts": date_birthday_list})
+
+    for i in range(8):
+        _date = current_date + timedelta(days=i)
+        date_list.append(_date)
+
+    for i in date_list:
+        context = ContactBook.objects.filter(birthday__month=i.month, birthday__day=i.day)
+        if context:
+            date_birthday_list.append(context)
+
+    return render(request, "contact_book/birthday_contacts.html", context={"contacts": date_birthday_list})
