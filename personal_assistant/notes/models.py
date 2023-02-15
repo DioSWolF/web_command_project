dd5a8772aef39229be244e61e5140d3ae6e1e656
Joohny21 <112joohny@gmail.com>
2023-02-15 11:48:59 +0100

    All taks requierements done

--- personal_assistant/notes/models.py
+++ personal_assistant/notes/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=25, null=False, unique=True)
+    name = models.CharField(max_length=32, null=False, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
@@ -13,9 +15,11 @@ class Note(models.Model):
     name = models.CharField(max_length=64, null=False)
     description = models.CharField(max_length=4096, null=False)
     created = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, max_length=15)
+    tags = models.ManyToManyField(Tag, max_length=5)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def extract_tags(self):
         pass
+
     def __str__(self):
         return f"{self.pk}♂{self.name}♂{self.description}♂{self.tags}♂{self.created}"
--- personal_assistant/notes/templatetags/extract_tags.py
+++ personal_assistant/notes/templatetags/extract_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 def tags(note_tags):
-    return ', '.join([str(name) for name in note_tags.all()])
+    return [str(name) for name in note_tags.all()]
 
 
 register.filter('tags', tags)
